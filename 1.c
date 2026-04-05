3bf12465510e568c41dd95f754ec3b5ec9d46e11
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-04-05 22:52:20 +0530

    assignment1

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Arun Aacharya\n");
+    printf("Date of Birth: 02/01/1999\n");
+    printf("Phone Number: +91 3937293645\n");
+
+    return 0;
+}
